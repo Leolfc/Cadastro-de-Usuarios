fc311b3f62fa8336baf5cc23c03f19f4aa046a91
Leolfc <leonardofeliciano81@gmail.com>
2025-10-23 16:19:04 -0300

    estilizando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
 import "./App.css";
-import { Container, Title } from "./pages/Home/styles";
-
+import {
+  Button,
+  Container,
+  ContainerInputs,
+  Form,
+  Input,
+  InputLabel,
+  Title,
+  TopBackground,
+} from "./pages/Home/styles";
+import UsersImage from "../src/assets/users.png"
 function Home() {
   return (
     <Container>
-      <Title>Cadastro de Usuários</Title>
+      <TopBackground>
+        <img src={UsersImage} alt="Imagens dos usuários" />
+      </TopBackground>
+      <Form>
+        <Title>Cadastrar Usuários</Title>
+        <ContainerInputs>
+          <div>
+            <InputLabel>
+              Nome
+              <span>*</span>
+            </InputLabel>
+
+            <Input type="text" placeholder="Nome do usuário" />
+            <InputLabel>
+              Idade
+              <span>*</span>
+            </InputLabel>
+            <Input type="number" placeholder="Idade do usuário" />
+          </div>
+          <InputLabel>
+              E-mail
+              <span>*</span>
+            </InputLabel>
+          <Input type="email" placeholder="E-mail do usuário" />
+        </ContainerInputs>
+        <Button>Cadastrar Usuário</Button>
+      </Form>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,6 +5,48 @@ color: red;
 `
 
 export const Container = styled.div`
-background-color: blue;
+background-color: #181f36;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+gap:20px;
 
 `
+
+export const UsersImage = styled.img`
+max-width: 400px;
+`
+
+
+export const TopBackground= styled.div`
+display: flex;
+background:linear-gradient(to right, #fe7e5d, #7f3b41);
+height: 30vh;
+width:90vw;
+border-radius: 30px;
+align-items: center;
+justify-content: center;
+img{
+    max-width:100%;
+    max-height:100%;
+}
+`
+
+export const Form = styled.form`
+`
+export const ContainerInputs = styled.div`
+
+`
+export const InputLabel = styled.label`
+
+`
+
+
+export const Input = styled.input`
+
+`
+
+export const Button = styled.button`
+
+`   

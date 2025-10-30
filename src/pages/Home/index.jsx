3b9fc108e597f923b78fd2edb939a486c05eba24
Leolfc <leonardofeliciano81@gmail.com>
2025-10-30 16:38:42 -0300

    trabalhando backend

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,2 +1,77 @@
+import { useRef } from "react";
+import api from "../../services/api.js";
 
+import "./styles.js";
+import {
+  Button,
+  Container,
+  ContainerInputs,
+  Form,
+  Input,
+  InputLabel,
+  Title,
+  TopBackground,
+} from "./styles";
+import UsersImage from "../../assets/users.png";
 
+function Home() {
+  const inputName = useRef(); //usando o useRef para pegar o valor do input
+  const inputIdade = useRef();
+  const inputEmail = useRef();
+  async function registerNewUser() {
+    const data = await api.post("/usuarios", {
+      email: inputEmail.current.value,
+      age: parseInt(inputIdade.current.value),
+      name: inputName.current.value,
+    });
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <TopBackground>
+        <img src={UsersImage} alt="Imagens dos usuários" />
+      </TopBackground>
+      <Form>
+        <Title>Cadastrar Usuários</Title>
+        <ContainerInputs>
+          <div>
+            <InputLabel>
+              Nome
+              <span> *</span>
+            </InputLabel>
+
+            <Input type="text" placeholder="Nome do usuário" ref={inputName} />
+            <InputLabel>
+              Idade
+              <span> *</span>
+            </InputLabel>
+            <Input
+              type="number"
+              placeholder="Idade do usuário"
+              ref={inputIdade}
+            />
+          </div>
+        </ContainerInputs>
+        <div
+          style={{ width: "100%", display: "flex", justifyContent: "center" }}
+        >
+          <InputLabel>
+            E-mail
+            <span>*</span>
+          </InputLabel>
+          <Input
+            type="email"
+            placeholder="E-mail do usuário"
+            ref={inputEmail}
+          />
+        </div>
+        <Button type="button" onClick={registerNewUser}>
+          Cadastrar Usuário
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,52 +1,82 @@
 import styled from "styled-components";
 
 export const Title = styled.h2`
-color: red;
-`
+  color: #ffff;
+  text-align: center;
+  font-size: 38px;
+  font-weight: 600;
+  font-style: normal;
+`;
 
 export const Container = styled.div`
-background-color: #181f36;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: space-evenly;
-gap:20px;
-
-`
+  background-color: #181f36;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 20px;
+  min-height: 100vh;
+`;
 
 export const UsersImage = styled.img`
-max-width: 400px;
-`
+  max-width: 400px;
+`;
 
-
-export const TopBackground= styled.div`
-display: flex;
-background:linear-gradient(to right, #fe7e5d, #7f3b41);
-height: 30vh;
-width:90vw;
-border-radius: 30px;
-align-items: center;
-justify-content: center;
-img{
-    max-width:100%;
-    max-height:100%;
-}
-`
+export const TopBackground = styled.div`
+  display: flex;
+  background: linear-gradient(to right, #fe7e5d, #7f3b41);
+  height: 30vh;
+  max-width: 90vw;
+  border-radius: 30px;
+  align-items: center;
+  justify-content: center;
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
 
 export const Form = styled.form`
-`
-export const ContainerInputs = styled.div`
-
-`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: center;
+  max-width: 500px;
+  gap: 20px;
+`;
+export const ContainerInputs = styled.div``;
 export const InputLabel = styled.label`
-
-`
-
+  color: #ffff;
+  font-size: 12px;
+  font-weight: 500;
+  margin-left: 5px;
+  span {
+    color: red;
+    font-weight: bold;
+    margin: 5px;
+  }
+`;
 
 export const Input = styled.input`
-
-`
+  border: 1px solid #d2dae2;
+  border-radius: 10px;
+  background-color: #353333ff;
+  padding: 12px 20px;
+  outline: none;
+  color: #ffff;
+`;
 
 export const Button = styled.button`
-
-`   
+  border: none;
+  background: linear-gradient(180deg, #fe7e5d 0%, #ff6378 100%);
+  padding: 16px 32px;
+  width: fit-content;
+  border-radius: 30px;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.6;
+  }
+`;
